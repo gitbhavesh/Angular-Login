32d9795d98c8b0d093aa9c3755a22c713394a0e2
gitbhavesh <bkssadhu07@gmail.com>
2017-02-14 15:25:46 +0530

    basicLoginFinalCommit

---
+++ JS/controller.js
@@ -0,0 +1,36 @@
+var app = angular.module('mainApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'login.html'
+        })
+        .when('/dashboard', {
+            resolve: {
+                "check": function ($location, $rootScope) {
+                    if (!$rootScope.loggedIn) {
+                        $location.path('/');
+                    }
+                }
+            },
+            templateUrl: 'dashboard.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+app.controller('loginCTRL', function ($scope, $location, $rootScope) {
+    $scope.submit = function () {
+        //       $rootscope.thisname = "afsa"; //anything with this rootescope remians accessible throughout the page in any controller.
+//        $rootscope.uname = $scope.username;
+//        $rootscope.pass = $scope.password;
+
+        if ($scope.username === 'admin' && $scope.password === 'admin') {
+            $rootScope.loggedIn = true;
+            $location.path('/dashboard');
+        } else {
+            alert('Wrong Entry');
+        }
+    };
+});
